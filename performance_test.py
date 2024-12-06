fb931818d9e8194873d19a17da9a4d251e40610a
Aditya Patil <ap4078@rit.edu>
2024-12-05 20:11:31 -0500

    Update performance_test.py

--- performance_test.py
+++ performance_test.py
@@ -3,9 +3,6 @@ import subprocess
 #import time
 from timeit import default_timer as timer
 
-
-
-#directory_path = "./files"
 directory_path = "."
 files = os.listdir(directory_path)
 os.system('mkdir signatures')
@@ -17,16 +14,12 @@ for file in files:
     file_size = os.path.getsize(file_path)
     print(f'File size : {file_size}')
     start = timer()
-    #result = os.system(f'./sign {file_path}')    
     result = os.system(f'./sign {file_path}') #Comment or uncomment the result* variable lines as you need to test RSA, MLSDA signing time or both together.
     result2 = os.system(f'./MLDSA_SIG_GEN_V2 {file_path} ./private_key.sk')  
     end = timer()
     print(f'#### Time taken to sign : {round((end - start)*1000,2)}')
 
     print(f"\n####### Verifying : {file_path}\n")
-    #os.system(f'cp signature.bin ./signatures/{file_path}_signature.bin')  
-    #os.system(f'cp signature.bin ./signatures/{file_path}_signature.sig')  
-    #print(result)
     start1 = timer()
     result3 = os.system(f'./rsa_verify {file_path}') #Comment or uncomment the result* variable lines as you need to test RSA, MLSDA sign verification time or both together.
     result4 = os.system(f'./MLDSA_SIG_VERIFY {file_path} ./signature.sig ./public_key.pk')  
